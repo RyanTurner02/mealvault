af9845a005dcfda28f099efba6bbe9428e7aa545
Ryan Turner <ryanturner02@protonmail.com>
2024-11-05 12:27:15 -0500

    Add navbar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,28 +1,13 @@
-'use client'
-
-import React, { useEffect, useState } from "react";
+import Header from "@/app/components/header";
 
 export default function Home() {
-  const [message, setMessage] = useState("Loading");
-  const [people, setPeople] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:8080/api/home")
-      .then((response) => response.json())
-      .then((data) => {
-        setMessage(data.message);
-        setPeople(data.people);
-      });
-  }, []);
-
-  return(
-    <div>
-      {message}
-      {
-        people.map((person, index) => (
-          <div key={index}>{person}</div>
-        ))
-      }
-    </div>
+  return (
+    <>
+      <Header />
+      <main className="">
+        <h1>Hi</h1>
+        <p>Poop</p>
+      </main>
+    </>
   );
 }
