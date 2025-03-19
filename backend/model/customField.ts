a1bccf4bd18ad8aea4d2dc1e5e754b51eb6b421f
Ryan Turner <ryanturner02@protonmail.com>
2025-03-19 18:42:28 -0500

    Create customField.ts

---
+++ backend/model/customField.ts
@@ -0,0 +1,15 @@
+class CustomField {
+  private _fieldId: number;
+  private _recipeId: number;
+  private _fieldName: string;
+  private _fieldType: number;
+  private _fieldText: string;
+
+  constructor(fieldId: number, recipeId: number, fieldName: string, fieldType: number, fieldText: string) {
+    this._fieldId = fieldId;
+    this._recipeId = recipeId;
+    this._fieldName = fieldName;
+    this._fieldType = fieldType;
+    this._fieldText = fieldText;
+  }
+}
