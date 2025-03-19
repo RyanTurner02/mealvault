83ef4e81952a896a021843361af09f9de7591e06
Ryan Turner <ryanturner02@protonmail.com>
2025-03-19 18:44:11 -0500

    Create instruction.ts

---
+++ backend/model/instruction.ts
@@ -0,0 +1,11 @@
+class Instruction {
+  private _instructionId: number;
+  private _recipeId: number;
+  private _instructionText: string;
+
+  constructor(instructionId: number, recipeId: number, instructionText: string) {
+    this._instructionId = instructionId;
+    this._recipeId = recipeId;
+    this._instructionText = instructionText;
+  }
+}
