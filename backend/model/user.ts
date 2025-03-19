4c9e9a9933a6f8b2441a08bce525d4c53e9b9767
Ryan Turner <ryanturner02@protonmail.com>
2025-03-19 18:33:40 -0500

    Create user.ts

---
+++ backend/model/user.ts
@@ -0,0 +1,17 @@
+class User {
+  private _id: number;
+  private _name: string;
+  private _password: string;
+  private _email: string;
+  private _creationDate: Date;
+  private _updatedDate: Date;
+
+  constructor(id: number, name: string, password: string, email: string, creationDate: Date, updatedDate: Date) {
+    this._id = id;
+    this._name = name;
+    this._password = password;
+    this._email = email;
+    this._creationDate = creationDate;
+    this._updatedDate = updatedDate;
+  }
+}
