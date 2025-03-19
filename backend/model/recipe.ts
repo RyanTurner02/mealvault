ee06b0ac1006b8e5c3cf04dfc05013d6c32c510e
Ryan Turner <ryanturner02@protonmail.com>
2025-03-19 18:46:23 -0500

    Create recipe.ts

---
+++ backend/model/recipe.ts
@@ -0,0 +1,15 @@
+class Recipe {
+  private _recipeId: number;
+  private _userId: number;
+  private _recipeName: string;
+  private _dateCreated: Date;
+  private _dateUpdated: Date;
+
+  constructor(recipeId: number, userId: number, recipeName: string, dateCreated: Date, dateUpdated: Date) {
+    this._recipeId = recipeId;
+    this._userId = userId;
+    this._recipeName = recipeName;
+    this._dateCreated = dateCreated;
+    this._dateUpdated = dateUpdated;
+  }
+}
