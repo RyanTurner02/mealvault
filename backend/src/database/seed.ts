a2267704dc21b57855410026d63b9dafaab747c7
Ryan Turner <ryanturner02@protonmail.com>
2025-03-22 09:50:50 -0500

    Implement createUser()

--- backend/src/database/seed.ts
+++ backend/src/database/seed.ts
@@ -1,1 +1,28 @@
+import { faker } from '@faker-js/faker';
+import { User } from '@/model/user';
+import { Recipe } from '@/model/recipe';
+import { Instruction } from '@/model/instruction';
+import { CustomField } from '@/model/customField';
 
+const createUser = (userId: number): void => {
+    const name: string = faker.internet.displayName();
+    const password: string = faker.internet.password();
+    const email: string = faker.internet.exampleEmail();
+    const creationDate: Date = new Date();
+    const updatedDate: Date = creationDate;
+    const user: User = new User(userId, name, password, email, creationDate, updatedDate);
+}
+
+const createRecipe = (): void => {
+
+}
+
+const createInstruction = (): void => {
+
+}
+
+const createCustomField = (): void => {
+
+}
+
+createUser(1);
