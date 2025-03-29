bd549ffe49624ea00b1d9a85444a588081ccf673
Ryan Turner <ryanturner02@protonmail.com>
2025-03-29 10:05:44 -0500

    Add dashboard placeholder text

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,9 +4,12 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <main className="">
-        <h1>Hi</h1>
-        <p>Poop</p>
+      <main>
+        <h1 className="text-4xl font-bold text-center mt-2 mb-5">Dashboard</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate soluta error obcaecati ad dolorum nostrum, iure optio aspernatur neque mollitia, ipsam nobis. Deleniti neque est voluptate sint excepturi odio voluptas.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate soluta error obcaecati ad dolorum nostrum, iure optio aspernatur neque mollitia, ipsam nobis. Deleniti neque est voluptate sint excepturi odio voluptas.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate soluta error obcaecati ad dolorum nostrum, iure optio aspernatur neque mollitia, ipsam nobis. Deleniti neque est voluptate sint excepturi odio voluptas.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate soluta error obcaecati ad dolorum nostrum, iure optio aspernatur neque mollitia, ipsam nobis. Deleniti neque est voluptate sint excepturi odio voluptas.</p>
       </main>
     </div>
   );
