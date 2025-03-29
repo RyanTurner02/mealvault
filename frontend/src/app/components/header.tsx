33a82c811c5c7d91542964c6feb8a0c4d65aac9e
Ryan Turner <ryanturner02@protonmail.com>
2025-03-28 20:57:26 -0500

    Update header

--- frontend/src/app/components/header.tsx
+++ frontend/src/app/components/header.tsx
@@ -4,15 +4,15 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="border-b">
-      <nav className="flex justify-between py-2 mx-24">
+    <header className="border-b bg-blue-700 text-white p-2">
+      <nav className="flex flex-wrap justify-between">
         <div>
-          <Link className="mx-2" href="/">Mealvault</Link>
+          <Link className="mx-2 text-2xl font-bold" href="/">Mealvault</Link>
         </div>
-        <div>
+        <div className="my-auto">
           <Link className="mx-2" href="/login">Login</Link>
           <Link className="mx-2" href="/register">Register</Link>
-          <Link className="mx-2" href="/logout">Sign out</Link>
+          <Link className="mx-2" href="/logout">Logout</Link>
         </div>
       </nav>
     </header>
