8cc3438a7bbaa7bb6c7eeff8c00beefa6ae70483
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-05-17 09:54:25 -0500

    Add middleware directory

--- api/jest.config.js
+++ api/jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
   moduleNameMapper: {
     '@controller/(.*)': '<rootDir>/src/controller/$1',
     '@service/(.*)': '<rootDir>/src/service/$1',
-    '@repository/(.*)': '<rootDir>/src/repository/$1'
+    '@repository/(.*)': '<rootDir>/src/repository/$1',
+    '@middleware/(.*)': '<rootDir>/src/middleware/$1',
   }
 };
