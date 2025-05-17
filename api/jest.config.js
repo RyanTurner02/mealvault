6196410621f975c2a6eb12268510d50e20a2fc26
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-05-17 16:56:26 -0500

    Include model folder

--- api/jest.config.js
+++ api/jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "^.+\.tsx?$": ["ts-jest",{}],
   },
   moduleNameMapper: {
+    '@model/(.*)': '<rootDir>/src/model/$1',
     '@controller/(.*)': '<rootDir>/src/controller/$1',
     '@service/(.*)': '<rootDir>/src/service/$1',
     '@repository/(.*)': '<rootDir>/src/repository/$1',
