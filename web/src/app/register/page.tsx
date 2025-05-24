ef9753ba92b08ad8315f83281ceb78855ff64cba
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-05-24 17:36:35 -0500

    Redirect user upon successful account creation

--- web/src/app/register/page.tsx
+++ web/src/app/register/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import Header from "@/app/components/header";
+import { useRouter } from "next/navigation";
 import { ChangeEventHandler, FormEventHandler, useState } from "react";
 
 export default function Register() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const router = useRouter();
 
   const updateName: ChangeEventHandler<HTMLInputElement> = (e) => {
     setName(e.target.value);
@@ -25,13 +27,18 @@ export default function Register() {
 
     const response = await fetch(`${process.env.NEXT_PUBLIC_URL}:${process.env.NEXT_PUBLIC_API_PORT}/api/user/create`, {
       method: "POST",
+      credentials: "include",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({ name, email, password })
     });
-
-    return await response.json();
+    
+    if (response.ok) {
+      router.push("/");
+    } else {
+      console.log("Invlalid account creation");
+    }
   }
 
   return (
@@ -39,8 +46,8 @@ export default function Register() {
       <Header />
       <main>
         <div className="flex justify-center">
-          <div className="border-2 border-blue-300 rounded-lg w-11/12 sm:w-8/12 md:w-8/12 lg:w-6/12 xl:w-4/12 mt-5 p-5">
-            <h1 className="text-4xl font-bold text-center mb-3">Register</h1>
+          <div className="w-11/12 p-5 mt-5 border-2 border-blue-300 rounded-lg sm:w-8/12 md:w-8/12 lg:w-6/12 xl:w-4/12">
+            <h1 className="mb-3 text-4xl font-bold text-center">Register</h1>
             <form className="flex flex-col" onSubmit={handleSubmit}>
               <div className="flex flex-col mb-3">
                 <label htmlFor="name">Name</label>
@@ -54,7 +61,7 @@ export default function Register() {
                 <label htmlFor="password">Password</label>
                 <input className="text-field" id="password" type="password" placeholder="Password" value={password} onChange={updatePassword} required />
               </div>
-              <button className="rounded-full text-white bg-blue-500 hover:bg-blue-600 px-3 py-2">Create Account</button>
+              <button className="px-3 py-2 text-white bg-blue-500 rounded-full hover:bg-blue-600">Create Account</button>
             </form>
           </div>
         </div>
