b4c522f83577ab4d7e1e62e1b6489949583691e2
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-05-24 09:52:10 -0500

    Update tailwind order

--- web/src/app/login/page.tsx
+++ web/src/app/login/page.tsx
@@ -34,8 +34,8 @@ export default function Login() {
       <Header />
       <main>
         <div className="flex justify-center">
-          <div className="border-2 border-blue-300 rounded-lg w-11/12 sm:w-8/12 md:w-8/12 lg:w-6/12 xl:w-4/12 mt-5 p-5">
-            <h1 className="text-4xl font-bold text-center mb-3">Login</h1>
+          <div className="w-11/12 p-5 mt-5 border-2 border-blue-300 rounded-lg sm:w-8/12 md:w-8/12 lg:w-6/12 xl:w-4/12">
+            <h1 className="mb-3 text-4xl font-bold text-center">Login</h1>
             <form className="flex flex-col" onSubmit={handleSubmit}>
               <div className="flex flex-col mb-3">
                 <label htmlFor="email">Email Address</label>
@@ -45,7 +45,7 @@ export default function Login() {
                 <label htmlFor="password">Password</label>
                 <input className="text-field" id="password" type="password" placeholder="Password" value={password} onChange={updatePassword} required />
               </div>
-              <button className="rounded-full text-white bg-blue-500 hover:bg-blue-600 px-3 py-2">Login</button>
+              <button className="px-3 py-2 text-white bg-blue-500 rounded-full hover:bg-blue-600">Login</button>
             </form>
           </div>
         </div>
