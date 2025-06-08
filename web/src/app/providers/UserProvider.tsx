c7e963f96acecb6e4afe1af3f2cf1aff191bba34
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-06-08 18:26:07 -0500

    Implement FetchUser in the UserProvider

--- web/src/app/providers/UserProvider.tsx
+++ web/src/app/providers/UserProvider.tsx
@@ -3,37 +3,35 @@
 import { useEffect, useMemo, useState } from "react";
 import { UserContext } from "@/app/contexts/UserContext";
 import { User } from "@/app/types/user";
+import { FetchUser } from "@/app/utils/FetchAuth";
 
 export const UserProvider = ({ children }: { children: React.ReactNode }) => {
     const [user, setUser] = useState<User | null>(null);
     const [isLoading, setIsLoading] = useState(true);
     const contextValue = useMemo(() => ({ user, isLoading }), [user, isLoading]);
-    const url = `${process.env.NEXT_PUBLIC_URL}:${process.env.NEXT_PUBLIC_API_PORT}/api/user/me`;
     
-    useEffect(() => {
-        const fetchUser = async () => {
-            const res = await fetch(url, {
-                method: "GET",
-                credentials: "include",
-                headers: {
-                    "Content-Type": "application/json"
-                },
+    const refreshUser = async() => {
+        setIsLoading(true);
+
+        const res = await FetchUser();
+
+        if (res && res.ok) {
+            const data = await res.json();
+            setUser({
+                id: data.user._id,
+                name: data.user._name,
+                email: data.user._email
             });
-
-            if (res.ok) {
-                const data = await res.json();
-                setUser({
-                    id: data.user._id,
-                    name: data.user._name,
-                    email: data.user._email
-                });
-            }
-
-            setIsLoading(false);
+        } else {
+            setUser(null);
         }
+        setIsLoading(false);
+    }
 
-        fetchUser();
-    }, [url]);
+    useEffect(() => {
+        console.log("refreshing user...");
+        refreshUser();
+    }, []);
 
     return (
         <UserContext.Provider value={contextValue}>
