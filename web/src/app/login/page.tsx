6f76c019dea0bd3e536bc3ec7b4c9c23c3d5bc69
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-06-08 16:59:30 -0500

    Update loading state

--- web/src/app/login/page.tsx
+++ web/src/app/login/page.tsx
@@ -1,15 +1,18 @@
 "use client";
 
 import Header from "@/app/components/header";
-import { redirect, useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 import { ChangeEventHandler, useState } from "react";
 import { useUserContext } from "@/app/hooks/UserHook";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  let user = useUserContext();
-  const router = useRouter();
+  const userContext = useUserContext();
+
+  if (userContext?.user) {
+    redirect("/");
+  }
 
   const updateEmail: ChangeEventHandler<HTMLInputElement> = (e) => {
     setEmail(e.target.value);
@@ -41,25 +44,27 @@ export default function Login() {
 
   return (
     <>
-      <Header />
-      <main>
-        <div className="flex justify-center">
-          <div className="w-11/12 p-5 mt-5 border-2 border-blue-300 rounded-lg sm:w-8/12 md:w-8/12 lg:w-6/12 xl:w-4/12">
-            <h1 className="mb-3 text-4xl font-bold text-center">Login</h1>
-            <form className="flex flex-col" onSubmit={handleSubmit}>
-              <div className="flex flex-col mb-3">
-                <label htmlFor="email">Email Address</label>
-                <input className="text-field" id="email" type="text" placeholder="Email Address" value={email} onChange={updateEmail} required />
-              </div>
-              <div className="flex flex-col mb-7">
-                <label htmlFor="password">Password</label>
-                <input className="text-field" id="password" type="password" placeholder="Password" value={password} onChange={updatePassword} required />
-              </div>
-              <button className="px-3 py-2 text-white bg-blue-500 rounded-full hover:bg-blue-600">Login</button>
-            </form>
+      <Header /> 
+      {!userContext?.isLoading && (
+        <main>
+          <div className="flex justify-center">
+            <div className="w-11/12 p-5 mt-5 border-2 border-blue-300 rounded-lg sm:w-8/12 md:w-8/12 lg:w-6/12 xl:w-4/12">
+              <h1 className="mb-3 text-4xl font-bold text-center">Login</h1>
+              <form className="flex flex-col" onSubmit={handleSubmit}>
+                <div className="flex flex-col mb-3">
+                  <label htmlFor="email">Email Address</label>
+                  <input className="text-field" id="email" type="text" placeholder="Email Address" value={email} onChange={updateEmail} required />
+                </div>
+                <div className="flex flex-col mb-7">
+                  <label htmlFor="password">Password</label>
+                  <input className="text-field" id="password" type="password" placeholder="Password" value={password} onChange={updatePassword} required />
+                </div>
+                <button className="px-3 py-2 text-white bg-blue-500 rounded-full hover:bg-blue-600">Login</button>
+              </form>
+            </div>
           </div>
-        </div>
-      </main>
+        </main>
+      )}
     </>
   );
 }
