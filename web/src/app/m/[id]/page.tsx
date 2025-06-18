2b665b3ecca7157078402c510cec2b1dae73980f
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-06-17 19:12:21 -0500

    Create recipe page

---
+++ web/src/app/m/[id]/page.tsx
@@ -0,0 +1,14 @@
+import Header from "@/app/components/header";
+
+export default function Meal() {
+  return (
+    <div className="min-h-svh">
+      <Header />
+      <div className="flex items-center justify-center w-full p-6 md:p-10">
+        <div className="w-full max-w-sm">
+          <p>Hello World!</p>
+        </div>
+      </div>
+    </div>
+  );
+}
