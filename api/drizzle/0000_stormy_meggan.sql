ebd65d9ad69b067ed665c8d23b4f63abaf58cf6b
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-06-25 20:31:59 -0500

    Install Drizzle and begin migration

---
+++ api/drizzle/0000_stormy_meggan.sql
@@ -0,0 +1,54 @@
+-- Current sql file was generated after introspecting the database
+-- If you want to run this migration please uncomment this code before executing migrations
+/*
+CREATE TABLE `custom_field` (
+	`field_id` int AUTO_INCREMENT NOT NULL,
+	`recipe_id` int NOT NULL,
+	`field_name` varchar(255),
+	`field_type` int,
+	`field_text` text,
+	`date_created` datetime DEFAULT (CURRENT_TIMESTAMP),
+	`date_updated` datetime DEFAULT (CURRENT_TIMESTAMP),
+	`date_deleted` datetime,
+	CONSTRAINT `custom_field_field_id` PRIMARY KEY(`field_id`)
+);
+--> statement-breakpoint
+CREATE TABLE `instruction` (
+	`instruction_id` int AUTO_INCREMENT NOT NULL,
+	`recipe_id` int NOT NULL,
+	`instruction_text` text,
+	`date_created` datetime DEFAULT (CURRENT_TIMESTAMP),
+	`date_updated` datetime DEFAULT (CURRENT_TIMESTAMP),
+	`date_deleted` datetime,
+	CONSTRAINT `instruction_instruction_id` PRIMARY KEY(`instruction_id`)
+);
+--> statement-breakpoint
+CREATE TABLE `recipe` (
+	`recipe_id` int AUTO_INCREMENT NOT NULL,
+	`user_id` int NOT NULL,
+	`recipe_name` varchar(255) NOT NULL,
+	`date_created` datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+	`date_updated` datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+	`date_deleted` datetime,
+	CONSTRAINT `recipe_recipe_id` PRIMARY KEY(`recipe_id`)
+);
+--> statement-breakpoint
+CREATE TABLE `user` (
+	`user_id` int AUTO_INCREMENT NOT NULL,
+	`user_name` varchar(255) NOT NULL,
+	`user_password` varchar(255) NOT NULL,
+	`user_email` varchar(255),
+	`date_created` datetime DEFAULT (CURRENT_TIMESTAMP),
+	`date_updated` datetime DEFAULT (CURRENT_TIMESTAMP),
+	`date_deleted` datetime,
+	CONSTRAINT `user_user_id` PRIMARY KEY(`user_id`),
+	CONSTRAINT `user_email` UNIQUE(`user_email`)
+);
+--> statement-breakpoint
+ALTER TABLE `custom_field` ADD CONSTRAINT `custom_field_ibfk_1` FOREIGN KEY (`recipe_id`) REFERENCES `recipe`(`recipe_id`) ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE `instruction` ADD CONSTRAINT `instruction_ibfk_1` FOREIGN KEY (`recipe_id`) REFERENCES `recipe`(`recipe_id`) ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE `recipe` ADD CONSTRAINT `recipe_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user`(`user_id`) ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+CREATE INDEX `recipe_id` ON `custom_field` (`recipe_id`);--> statement-breakpoint
+CREATE INDEX `recipe_id` ON `instruction` (`recipe_id`);--> statement-breakpoint
+CREATE INDEX `user_id` ON `recipe` (`user_id`);
+*/
---
+++ api/src/db/schema.ts
@@ -0,0 +1,57 @@
+import { mysqlTable, mysqlSchema, AnyMySqlColumn, index, foreignKey, primaryKey, int, varchar, text, datetime, unique } from "drizzle-orm/mysql-core"
+import { sql } from "drizzle-orm"
+
+export const customField = mysqlTable("custom_field", {
+	fieldId: int("field_id").autoincrement().notNull(),
+	recipeId: int("recipe_id").notNull().references(() => recipe.recipeId),
+	fieldName: varchar("field_name", { length: 255 }),
+	fieldType: int("field_type"),
+	fieldText: text("field_text"),
+	dateCreated: datetime("date_created", { mode: 'string'}).default(sql`(CURRENT_TIMESTAMP)`),
+	dateUpdated: datetime("date_updated", { mode: 'string'}).default(sql`(CURRENT_TIMESTAMP)`),
+	dateDeleted: datetime("date_deleted", { mode: 'string'}),
+},
+(table) => [
+	index("recipe_id").on(table.recipeId),
+	primaryKey({ columns: [table.fieldId], name: "custom_field_field_id"}),
+]);
+
+export const instruction = mysqlTable("instruction", {
+	instructionId: int("instruction_id").autoincrement().notNull(),
+	recipeId: int("recipe_id").notNull().references(() => recipe.recipeId),
+	instructionText: text("instruction_text"),
+	dateCreated: datetime("date_created", { mode: 'string'}).default(sql`(CURRENT_TIMESTAMP)`),
+	dateUpdated: datetime("date_updated", { mode: 'string'}).default(sql`(CURRENT_TIMESTAMP)`),
+	dateDeleted: datetime("date_deleted", { mode: 'string'}),
+},
+(table) => [
+	index("recipe_id").on(table.recipeId),
+	primaryKey({ columns: [table.instructionId], name: "instruction_instruction_id"}),
+]);
+
+export const recipe = mysqlTable("recipe", {
+	recipeId: int("recipe_id").autoincrement().notNull(),
+	userId: int("user_id").notNull().references(() => user.userId),
+	recipeName: varchar("recipe_name", { length: 255 }).notNull(),
+	dateCreated: datetime("date_created", { mode: 'string'}).default(sql`(CURRENT_TIMESTAMP)`).notNull(),
+	dateUpdated: datetime("date_updated", { mode: 'string'}).default(sql`(CURRENT_TIMESTAMP)`).notNull(),
+	dateDeleted: datetime("date_deleted", { mode: 'string'}),
+},
+(table) => [
+	index("user_id").on(table.userId),
+	primaryKey({ columns: [table.recipeId], name: "recipe_recipe_id"}),
+]);
+
+export const user = mysqlTable("user", {
+	userId: int("user_id").autoincrement().notNull(),
+	userName: varchar("user_name", { length: 255 }).notNull(),
+	userPassword: varchar("user_password", { length: 255 }).notNull(),
+	userEmail: varchar("user_email", { length: 255 }),
+	dateCreated: datetime("date_created", { mode: 'string'}).default(sql`(CURRENT_TIMESTAMP)`),
+	dateUpdated: datetime("date_updated", { mode: 'string'}).default(sql`(CURRENT_TIMESTAMP)`),
+	dateDeleted: datetime("date_deleted", { mode: 'string'}),
+},
+(table) => [
+	primaryKey({ columns: [table.userId], name: "user_user_id"}),
+	unique("user_email").on(table.userEmail),
+]);
