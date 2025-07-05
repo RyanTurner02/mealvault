e14613bb15cded213ec274f2e75b74ed0afc0310
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-07-05 11:53:10 -0500

    Create getUserByLogin() unit test

--- api/test/service/userService.test.ts
+++ api/test/service/userService.test.ts
@@ -1,13 +1,19 @@
 import { IUserRepository } from "@repository/userRepository";
 import { createUserService, IUserService } from "@service/userService";
-import { faker } from "@faker-js/faker/.";
+import { faker } from "@faker-js/faker";
 import User from "@model/user";
+import * as bcrypt from "bcrypt";
 
 const mockUserRepository: jest.Mocked<IUserRepository> = {
     getUserByEmail: jest.fn(),
     createUser: jest.fn(),
     getUser: jest.fn(),
 };
+
+jest.mock("bcrypt", () => ({
+    hash: jest.fn((password: string, salt: number) => Promise.resolve("hashedPassword")),
+    compareSync: jest.fn((password: string, hashedPassword: string) => password === hashedPassword),
+}));
 
 describe("UserService", () => {
     let userService: IUserService;
@@ -27,7 +33,26 @@ describe("UserService", () => {
     });
 
     it("gets user by email and password", async () => {
-        
+        const password = faker.internet.password();
+        const expected: User = new User(
+            1,
+            faker.internet.displayName(),
+            password,
+            faker.internet.exampleEmail(),
+        );
+
+        mockUserRepository.getUserByEmail.mockResolvedValue(expected);
+
+        const actual: User | null = await userService.getUserByLogin(expected.getEmail(), password);
+
+        expect(mockUserRepository.getUserByEmail).toHaveBeenCalledTimes(1);
+        expect(mockUserRepository.getUserByEmail).toHaveBeenCalledWith(expected.getEmail());
+
+        expect(bcrypt.compareSync).toHaveBeenCalledTimes(1);
+        expect(bcrypt.compareSync).toHaveBeenCalledWith(password, expected.getPassword());
+
+        expect(actual).not.toBeNull();
+        expect(actual).toEqual(expected);
     });
 
     it("gets user by id", async () => {
