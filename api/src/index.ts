690d6d4444c6556151f8afcdcd2e9f99407ff8d7
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-07-12 18:36:59 -0500

    Extract app setup into setup-app.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,41 +1,7 @@
-import express, { Express, Router } from "express";
-import cors, { CorsOptions } from "cors";
-import cookieParser from "cookie-parser";
-import { createUserRoute } from "@route/userRoute";
-import { createTokenRoute } from "@route/tokenRoute";
-import { createUserController, IUserController } from "@controller/userController";
-import { createUserService, IUserService } from "@service/userService";
-import { createUserRepository, IUserRepository } from "@repository/userRepository";
-import { db } from "@db/index";
-import { createTokenService, ITokenService } from "@service/tokenService";
-import { createCookieUtils, ICookieUtils } from "@utils/cookieUtils";
-import { createTokenController, ITokenController } from "@controller/tokenController";
+import { Express } from "express";
+import { setupApp } from "./setup-app";
 
-const app: Express = express();
-const port = process.env.PORT;
-const corsOptions: CorsOptions = {
-    origin: `${process.env.FRONTEND_URL}:${process.env.FRONTEND_PORT}`,
-    credentials: true,
-    optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
-app.use(cookieParser());
-app.use(express.json());
-
-const cookieUtils: ICookieUtils = createCookieUtils();
-
-const tokenService: ITokenService = createTokenService();
-const tokenController: ITokenController = createTokenController({ tokenService });
-
-const userRepository: IUserRepository = createUserRepository({ db });
-const userService: IUserService = createUserService({ userRepository });
-const userController: IUserController = createUserController({ userService, tokenService, cookieUtils });
-
-const userRoute: Router = createUserRoute({ userController });
-const tokenRoute: Router = createTokenRoute({ tokenController });
-
-app.use('/api/user/', userRoute);
-app.use('/api/token/', tokenRoute);
+const port: number = Number(process.env.PORT) || 3000;
+const app: Express = setupApp();
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ api/src/setup-app.ts
@@ -0,0 +1,43 @@
+import express, { Express, Router } from "express";
+import cors, { CorsOptions } from "cors";
+import cookieParser from "cookie-parser";
+import { createUserRoute } from "@route/userRoute";
+import { createTokenRoute } from "@route/tokenRoute";
+import { createUserController, IUserController } from "@controller/userController";
+import { createUserService, IUserService } from "@service/userService";
+import { createUserRepository, IUserRepository } from "@repository/userRepository";
+import { db } from "@db/index";
+import { createTokenService, ITokenService } from "@service/tokenService";
+import { createCookieUtils, ICookieUtils } from "@utils/cookieUtils";
+import { createTokenController, ITokenController } from "@controller/tokenController";
+
+export const setupApp = (): Express => {
+    const app: Express = express();
+
+    const corsOptions: CorsOptions = {
+        origin: `${process.env.FRONTEND_URL}:${process.env.FRONTEND_PORT}`,
+        credentials: true,
+        optionsSuccessStatus: 200,
+    };
+
+    app.use(cors(corsOptions));
+    app.use(cookieParser());
+    app.use(express.json());
+
+    const cookieUtils: ICookieUtils = createCookieUtils();
+
+    const tokenService: ITokenService = createTokenService();
+    const tokenController: ITokenController = createTokenController({ tokenService });
+
+    const userRepository: IUserRepository = createUserRepository({ db });
+    const userService: IUserService = createUserService({ userRepository });
+    const userController: IUserController = createUserController({ userService, tokenService, cookieUtils });
+
+    const userRoute: Router = createUserRoute({ userController });
+    const tokenRoute: Router = createTokenRoute({ tokenController });
+
+    app.use('/api/user/', userRoute);
+    app.use('/api/token/', tokenRoute);
+
+    return app;
+}
