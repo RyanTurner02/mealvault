60b8064ae21f94ffa6094b024bbc3afe78e91311
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-07-12 18:38:56 -0500

    Change default port number to 8080

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,7 @@
 import { Express } from "express";
 import { setupApp } from "./setup-app";
 
-const port: number = Number(process.env.PORT) || 3000;
+const port: number = Number(process.env.PORT) || 8080;
 const app: Express = setupApp();
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
