fbabe8e7816e47aebd024be25b4714e5f2168224
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-07-15 20:15:59 -0500

    Display modal on delete recipe button click

--- web/src/components/tables/recipe/columns.tsx
+++ web/src/components/tables/recipe/columns.tsx
@@ -3,26 +3,27 @@
 import * as z from "zod";
 import { ColumnDef } from "@tanstack/react-table";
 import { MoreHorizontal } from "lucide-react";
-
 import { Button } from "@/components/ui/button";
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { DeleteRecipeModal } from "@/components/modals/delete-recipe-modal";
+import { useState } from "react";
 
 const recipeSchema = z.object({
-    id: z.string(),
-    name: z.string(),
-    category: z.string(),
-    difficulty: z.string(),
-    prepTime: z.string(),
-    cookTime: z.string(),
-    servings: z.number(),
-    actions: z.string(),
+  id: z.string(),
+  name: z.string(),
+  category: z.string(),
+  difficulty: z.string(),
+  prepTime: z.string(),
+  cookTime: z.string(),
+  servings: z.number(),
+  actions: z.string(),
 });
 
 export type Recipe = z.infer<typeof recipeSchema>;
@@ -43,7 +44,8 @@ export const columns: ColumnDef<Recipe>[] = [
   {
     accessorKey: "prepTime",
     header: "Prep Time",
-  },  {
+  },
+  {
     accessorKey: "cookTime",
     header: "Cook Time",
   },
@@ -55,22 +57,37 @@ export const columns: ColumnDef<Recipe>[] = [
     accessorKey: "actions",
     header: "Actions",
     cell: ({ row }) => {
-        return (
-            <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                    <Button variant="ghost" className="w-8 h-8 p-0">
-                        <span className="sr-only">Open menu</span>
-                        <MoreHorizontal className="w-4 h-4" />
-                    </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem className="cursor-pointer">Edit</DropdownMenuItem>
-                    <DropdownMenuItem className="cursor-pointer">Delete</DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
-        );
+      const [open, setOpen] = useState(false);
+
+      return (
+        <>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="w-8 h-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="w-4 h-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem className="cursor-pointer">
+                Edit
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="cursor-pointer"
+                onSelect={(e) => {
+                  e.preventDefault();
+                  setOpen(true);
+                }}
+              >
+                Delete
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <DeleteRecipeModal recipeId={row.original.id} open={open} onOpenChange={setOpen} />
+        </>
+      );
     }
   },
 ];
