494ba987e2c9c0a71b90d155621933f4de8bb278
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-07-21 22:15:48 -0500

    Create hasAccessToken unit test for existing access tokens

---
+++ api/test/controller/token-controller.test.ts
@@ -0,0 +1,47 @@
+import { createTokenController, ITokenController } from "@controller/tokenController";
+import { ITokenService } from "@service/tokenService";
+import { Request, Response } from "express";
+import { createRequest, createResponse, MockRequest, MockResponse } from "node-mocks-http";
+import * as jwt from "jsonwebtoken";
+import "dotenv/config";
+
+describe("TokenController", () => {
+    let tokenController: ITokenController;
+
+    const mockTokenService: jest.Mocked<ITokenService> = {
+        generateAccessToken: jest.fn(),
+        generateRefreshToken: jest.fn(),
+    }
+
+    beforeAll(() => {
+        tokenController = createTokenController({
+            tokenService: mockTokenService
+        });
+    });
+
+    describe("hasAccessToken", () => {
+        it("has an access token", async () => {
+            const accessToken: string = jwt.sign(
+                { id: 0 },
+                process.env.ACCESS_TOKEN_SECRET!,
+                { expiresIn: '15m' });
+
+            const request: MockRequest<Request> = createRequest({
+                method: "GET",
+                url: "/api/token/has-access-token",
+                cookies: {
+                    access_token: accessToken,
+                },
+            });
+
+            const response: MockResponse<Response> = createResponse();
+
+            await tokenController.hasAccessToken(request, response);
+
+            expect(response.statusCode).toBe(200);
+            expect(response._getJSONData()).toEqual({
+                hasAccessToken: true
+            });
+        });
+    });
+});
