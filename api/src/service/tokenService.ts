417c2b09a6fd426ea542908cf38e0b4f93beb39e
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-07-21 22:44:29 -0500

    Update return types

--- api/src/service/tokenService.ts
+++ api/src/service/tokenService.ts
@@ -2,17 +2,17 @@ import { JwtPayload } from "@typings/token";
 import * as jwt from "jsonwebtoken";
 
 export interface ITokenService {
-    generateAccessToken(id: number): string | null;
-    generateRefreshToken(id: number): string | null;
+    generateAccessToken(id: number): string;
+    generateRefreshToken(id: number): string;
 }
 
 export const createTokenService = (): ITokenService => {
-    const generateAccessToken = (id: number): string | null => {
+    const generateAccessToken = (id: number): string => {
         const payload: JwtPayload = { id };
         return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET!, { expiresIn: '15m' });
     }
 
-    const generateRefreshToken = (id: number): string | null => {
+    const generateRefreshToken = (id: number): string => {
         const payload: JwtPayload = { id };
         return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET!, { expiresIn: '7d' });
     }
