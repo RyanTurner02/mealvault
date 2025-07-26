32627c33a4206b49ed5292c5936819d859b7fcee
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-07-26 14:13:22 -0500

    Drop instruction and custom_field tables

--- api/src/db/relations.ts
+++ api/src/db/relations.ts
@@ -1,26 +1,10 @@
 import { relations } from "drizzle-orm/relations";
-import { recipe, customField, instruction, user } from "./schema";
-
-export const customFieldRelations = relations(customField, ({one}) => ({
-	recipe: one(recipe, {
-		fields: [customField.recipeId],
-		references: [recipe.recipeId]
-	}),
-}));
+import { recipe, user } from "./schema";
 
 export const recipeRelations = relations(recipe, ({one, many}) => ({
-	customFields: many(customField),
-	instructions: many(instruction),
 	user: one(user, {
 		fields: [recipe.userId],
 		references: [user.userId]
-	}),
-}));
-
-export const instructionRelations = relations(instruction, ({one}) => ({
-	recipe: one(recipe, {
-		fields: [instruction.recipeId],
-		references: [recipe.recipeId]
 	}),
 }));
 
--- api/src/db/schema.ts
+++ api/src/db/schema.ts
@@ -1,34 +1,6 @@
 import { mysqlTable, mysqlSchema, AnyMySqlColumn, index, foreignKey, primaryKey, int, varchar, text, datetime, unique } from "drizzle-orm/mysql-core"
 import { sql } from "drizzle-orm"
 
-export const customField = mysqlTable("custom_field", {
-	fieldId: int("field_id").autoincrement().notNull(),
-	recipeId: int("recipe_id").notNull().references(() => recipe.recipeId),
-	fieldName: varchar("field_name", { length: 255 }),
-	fieldType: int("field_type"),
-	fieldText: text("field_text"),
-	dateCreated: datetime("date_created", { mode: 'string'}).default(sql`(CURRENT_TIMESTAMP)`),
-	dateUpdated: datetime("date_updated", { mode: 'string'}).default(sql`(CURRENT_TIMESTAMP)`),
-	dateDeleted: datetime("date_deleted", { mode: 'string'}),
-},
-(table) => [
-	index("recipe_id").on(table.recipeId),
-	primaryKey({ columns: [table.fieldId], name: "custom_field_field_id"}),
-]);
-
-export const instruction = mysqlTable("instruction", {
-	instructionId: int("instruction_id").autoincrement().notNull(),
-	recipeId: int("recipe_id").notNull().references(() => recipe.recipeId),
-	instructionText: text("instruction_text"),
-	dateCreated: datetime("date_created", { mode: 'string'}).default(sql`(CURRENT_TIMESTAMP)`),
-	dateUpdated: datetime("date_updated", { mode: 'string'}).default(sql`(CURRENT_TIMESTAMP)`),
-	dateDeleted: datetime("date_deleted", { mode: 'string'}),
-},
-(table) => [
-	index("recipe_id").on(table.recipeId),
-	primaryKey({ columns: [table.instructionId], name: "instruction_instruction_id"}),
-]);
-
 export const recipe = mysqlTable("recipe", {
 	recipeId: int("recipe_id").autoincrement().notNull(),
 	userId: int("user_id").notNull().references(() => user.userId),
