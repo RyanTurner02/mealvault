854ee9cff548e37155111fba7285dfbbda2527aa
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-07-31 20:35:02 -0500

    Update create recipe form

--- web/src/components/create-recipe-form.tsx
+++ web/src/components/create-recipe-form.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -6,171 +6,203 @@ import * as z from "zod";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
-    name: z.string().min(1, "Enter a recipe name"),
-    prepTime: z.string().min(1, "Enter prep time"),
-    cookTime: z.string().min(1, "Enter cook time"),
-    servings: z.string().min(1, "Enter number of servings"),
-    ingredients: z.string().min(1, "Enter ingredients"),
-    instructions: z.string().min(1, "Enter recipe instructions"),
-    externalLink: z.string().optional(),
+  name: z.string().min(1, "Enter a recipe name"),
+  prepTime: z.string().min(1, "Enter prep time"),
+  cookTime: z.string().min(1, "Enter cook time"),
+  servings: z.string().min(1, "Enter number of servings"),
+  ingredients: z.string().min(1, "Enter ingredients"),
+  instructions: z.string().min(1, "Enter recipe instructions"),
+  externalLink: z.string().optional(),
 });
 
 export const CreateRecipeForm = () => {
-    const router = useRouter();
-    
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            name: "",
-            prepTime: "",
-            cookTime: "",
-            servings: "",
-            ingredients: "",
-            instructions: "",
-        }
-    });
+  const router = useRouter();
 
-    const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_URL}:${process.env.NEXT_PUBLIC_API_PORT}/api/recipe/create`, {
-            method: "POST",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                name: values.name,
-                prepTime: values.prepTime,
-                cookTime: values.cookTime,
-                servings: values.servings,
-                ingredients: values.ingredients,
-                instructions: values.instructions,
-                externalLink: values?.externalLink
-            })
-        });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      prepTime: "",
+      cookTime: "",
+      servings: "",
+      ingredients: "",
+      instructions: "",
+    },
+  });
 
-        if (!response.ok) {
-            console.log("Invalid recipe");
-            return;
-        }
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_URL}:${process.env.NEXT_PUBLIC_API_PORT}/api/recipe/create`,
+      {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: values.name,
+          prepTime: values.prepTime,
+          cookTime: values.cookTime,
+          servings: values.servings,
+          ingredients: values.ingredients,
+          instructions: values.instructions,
+          externalLink: values?.externalLink,
+        }),
+      }
+    );
 
-        const result: number = await response.json();        
-        router.push(`/m/${result}`);
+    if (!response.ok) {
+      console.log("Invalid recipe");
+      return;
     }
 
-    return (
-        <Form { ... form }>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                <FormField
-                    control={form.control}
-                    name="name"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel className="text-2xl">Recipe Name</FormLabel>
-                            <FormControl>
-                                <Input placeholder="Recipe Name" { ... field } />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="prepTime"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel className="text-2xl">Prep Time</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Prep Time" { ... field} />
-                                </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="cookTime"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel className="text-2xl">Cook Time</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Cook Time" { ... field} />
-                                </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="servings"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel className="text-2xl">Servings</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Servings" { ... field} />
-                                </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="ingredients"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel className="text-2xl">Ingredients</FormLabel>
-                                <FormControl>
-                                    <Textarea placeholder="Ingredients" { ... field} />
-                                </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="instructions"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel className="text-2xl">Instructions</FormLabel>
-                                <FormControl>
-                                    <Textarea placeholder="Instructions" { ... field} />
-                                </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="externalLink"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel className="text-2xl">External Link</FormLabel>
-                            <FormControl>
-                                    <Input placeholder="https://mealvault.dev" { ... field} />
-                                </FormControl>
-                        </FormItem>
-                    )}
-                />
-                <div className="flex justify-evenly">
-                    <Button type="submit">Create</Button>
-                    <Link href="/">
-                        <Button type="button" variant="destructive">
-                            Cancel
-                        </Button>
-                    </Link>
-                </div>
-            </form>
+    const result: number = await response.json();
+    router.push(`/m/${result}`);
+  };
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-4xl font-bold text-center">
+          Create Recipe
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-2xl">
+                    Recipe Name <span className="text-red-500">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input placeholder="Recipe Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="prepTime"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-2xl">
+                    Prep Time <span className="text-red-500">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input placeholder="Prep Time" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="cookTime"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-2xl">
+                    Cook Time <span className="text-red-500">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input placeholder="Cook Time" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="servings"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-2xl">
+                    Servings <span className="text-red-500">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input placeholder="Servings" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="ingredients"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-2xl">
+                    Ingredients <span className="text-red-500">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="Ingredients" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="instructions"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-2xl">
+                    Instructions <span className="text-red-500">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="Instructions" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="externalLink"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-2xl">External Link</FormLabel>
+                  <FormControl>
+                    <Input placeholder="https://example.com" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <CardFooter className="flex justify-end p-0 pt-4 space-x-5">
+              <Button type="submit">Create</Button>
+              <Link href="/">
+                <Button type="button" variant="destructive">
+                  Cancel
+                </Button>
+              </Link>
+            </CardFooter>
+          </form>
         </Form>
-    );
-}
+      </CardContent>
+    </Card>
+  );
+};
