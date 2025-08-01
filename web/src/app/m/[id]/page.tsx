66f1089d7b4025aea65c669cef80bb03ca1e0d79
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-07-31 20:42:14 -0500

    Open external link in a new tab

--- web/src/app/m/[id]/page.tsx
+++ web/src/app/m/[id]/page.tsx
@@ -103,7 +103,7 @@ export default function Page() {
 
           <CardFooter>
             <span className="font-bold">External Link:</span>&nbsp;
-            <Link className="text-link" href={`${recipe?.externalLink}`}>
+            <Link className="text-link" href={`${recipe?.externalLink}`} target="_blank">
               {recipe?.externalLink}
             </Link>
           </CardFooter>
