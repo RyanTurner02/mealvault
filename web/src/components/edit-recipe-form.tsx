0e7a769539b26b70aac21f847385cfc263333001
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-08-01 19:03:26 -0500

    Extract recipe form schema; Move and update edit recipe page

--- web/src/components/edit-recipe-form.tsx
+++ web/src/components/edit-recipe-form.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -6,133 +6,165 @@ import * as z from "zod";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
-
-const formSchema = z.object({
-    name: z.string().min(1, "Enter a recipe name"),
-    prepTime: z.string().min(1, "Enter prep time"),
-    cookTime: z.string().min(1, "Enter cook time"),
-    ingredients: z.string().min(1, "Enter ingredients"),
-    instructions: z.string().min(1, "Enter recipe instructions"),
-    links: z.string().optional(),
-});
+import { recipeFormSchema } from "@/lib/schemas/recipe-form-schema";
 
 export const EditRecipeForm = () => {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            name: "",
-            prepTime: "",
-            cookTime: "",
-            ingredients: "",
-            instructions: "",
-        }
-    });
+  const onSubmit = (values: z.infer<typeof recipeFormSchema>) => {
+    console.log(values);
+  };
 
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
-        console.log(values);
-    }
+  const form = useForm<z.infer<typeof recipeFormSchema>>({
+    resolver: zodResolver(recipeFormSchema),
+    defaultValues: {
+      name: "",
+      prepTime: "",
+      cookTime: "",
+      servings: "",
+      ingredients: "",
+      instructions: "",
+    },
+  });
 
-    return (
-        <Form { ... form }>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                <FormField
-                    control={form.control}
-                    name="name"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel className="text-2xl">Recipe Name</FormLabel>
-                            <FormControl>
-                                <Input placeholder="Recipe Name" { ... field } />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="prepTime"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel className="text-2xl">Prep Time</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Prep Time" { ... field} />
-                                </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="cookTime"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel className="text-2xl">Cook Time</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="Cook Time" { ... field} />
-                                </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="ingredients"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel className="text-2xl">Ingredients</FormLabel>
-                                <FormControl>
-                                    <Textarea placeholder="Ingredients" { ... field} />
-                                </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="instructions"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel className="text-2xl">Instructions</FormLabel>
-                                <FormControl>
-                                    <Textarea placeholder="Instructions" { ... field} />
-                                </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="links"
-                    render={({ field }) => (
-                        <FormItem>
-                            <div className="flex justify-between">
-                                <FormLabel className="text-2xl">Links</FormLabel>
-                                <Button type="button" variant="secondary">Add Link</Button>
-                            </div>
-                                <FormControl>
-                                    <Input placeholder="Links" { ... field} />
-                                </FormControl>
-                        </FormItem>
-                    )}
-                />
-                <div className="flex justify-evenly">
-                    <Button type="submit">Save</Button>
-                    <Link href="/">
-                        <Button type="button" variant="destructive">
-                            Cancel
-                        </Button>
-                    </Link>
-                </div>
-            </form>
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-4xl font-bold text-center">
+          Edit Recipe
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-2xl">
+                    Recipe Name <span className="text-red-500">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input placeholder="Recipe Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="prepTime"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-2xl">
+                    Prep Time <span className="text-red-500">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input placeholder="Prep Time" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="cookTime"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-2xl">
+                    Cook Time <span className="text-red-500">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input placeholder="Cook Time" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="servings"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-2xl">
+                    Servings <span className="text-red-500">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input placeholder="Servings" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="ingredients"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-2xl">
+                    Ingredients <span className="text-red-500">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="Ingredients" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="instructions"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-2xl">
+                    Instructions <span className="text-red-500">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="Instructions" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="externalLink"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-2xl">External Link</FormLabel>
+                  <FormControl>
+                    <Input placeholder="https://example.com" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <CardFooter className="flex justify-end p-0 pt-4 space-x-5">
+              <Button type="submit">Save</Button>
+              <Link href="/">
+                <Button type="button" variant="destructive">
+                  Cancel
+                </Button>
+              </Link>
+            </CardFooter>
+          </form>
         </Form>
-    );
-}
+      </CardContent>
+    </Card>
+  );
+};
