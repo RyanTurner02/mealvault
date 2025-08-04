ca6fcb8d34f560004fbc4f85ad12f74f0e251844
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-08-03 22:31:46 -0500

    Add whitespace-pre-line for ingredients and instructions paragraphs

--- web/src/app/m/[id]/page.tsx
+++ web/src/app/m/[id]/page.tsx
@@ -49,7 +49,7 @@ export default function Page() {
                 <h2 className="mb-1 text-2xl font-bold text-center ">
                   Ingredients
                 </h2>
-                <p>{recipe?.ingredients}</p>
+                <p className="whitespace-pre-line">{recipe?.ingredients}</p>
               </div>
 
               <hr className="border-t border-gray-200" />
@@ -58,7 +58,7 @@ export default function Page() {
                 <h2 className="mb-1 text-2xl font-bold text-center">
                   Instructions
                 </h2>
-                <p>{recipe?.instructions}</p>
+                <p className="whitespace-pre-line">{recipe?.instructions}</p>
               </div>
             </div>
 
