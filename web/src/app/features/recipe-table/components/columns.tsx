338ad649198b835de1371180766a65c954cbb1c8
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-08-08 21:44:53 -0500

    Extract ActionsCell component to its own file

--- web/src/app/features/recipe-table/components/columns.tsx
+++ web/src/app/features/recipe-table/components/columns.tsx
@@ -1,74 +1,9 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
-import { MoreHorizontal } from "lucide-react";
-import { Button } from "@/app/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/app/components/ui/dropdown-menu";
-import { DeleteRecipeModal } from "@/app/features/recipe-table/components/delete-recipe-modal";
-import { useState } from "react";
-import Link from "next/link";
-import { deleteRecipe } from "@/app/features/delete-recipe/api/delete-recipe";
 import { Recipe } from "@/app/schemas/recipe-table-schema";
 import { RecipeNameCell } from "@/app/features/recipe-table/components/recipe-name-cell";
-
-interface ActionsCellProps {
-  recipeId: string;
-  onDelete: (id: string) => void;
-}
-
-const ActionsCell = ({ recipeId, onDelete }: ActionsCellProps) => {
-  const [open, setOpen] = useState(false);
-
-  return (
-    <>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" className="w-8 h-8 p-0">
-            <span className="sr-only">Open menu</span>
-            <MoreHorizontal className="w-4 h-4" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuLabel>Actions</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <Link href={`/m/${recipeId}/edit`}>
-            <DropdownMenuItem className="cursor-pointer">Edit</DropdownMenuItem>
-          </Link>
-          <DropdownMenuItem
-            className="cursor-pointer"
-            onSelect={(e) => {
-              e.preventDefault();
-              setOpen(true);
-            }}
-          >
-            Delete
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-      <DeleteRecipeModal
-        open={open}
-        onDeleteClicked={async () => {
-          const deletedRecipe: boolean = await deleteRecipe({ recipeId });
-
-          if (!deletedRecipe) {
-            return;
-          }
-
-          onDelete(recipeId);
-          setOpen(false);
-        }}
-        onOpenChange={setOpen}
-      />
-    </>
-  );
-};
+import { ActionsCell } from "@/app/features/recipe-table/components/actions-cell";
 
 export const columns = (
   onDelete: (id: string) => void
