436ea1a02c374bd1c8a9922eb8bcd605dc6264c4
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-08-08 21:39:51 -0500

    Extract Actions function

--- web/src/app/features/recipe-table/components/columns.tsx
+++ web/src/app/features/recipe-table/components/columns.tsx
@@ -30,6 +30,59 @@ const RecipeNameCell = ({ recipeId, recipeName }: RecipeNameCellProps) => {
   );
 };
 
+interface ActionsCellProps {
+  recipeId: string;
+  onDelete: (id: string) => void;
+}
+
+const ActionsCell = ({ recipeId, onDelete }: ActionsCellProps) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="w-8 h-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="w-4 h-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <Link href={`/m/${recipeId}/edit`}>
+            <DropdownMenuItem className="cursor-pointer">Edit</DropdownMenuItem>
+          </Link>
+          <DropdownMenuItem
+            className="cursor-pointer"
+            onSelect={(e) => {
+              e.preventDefault();
+              setOpen(true);
+            }}
+          >
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DeleteRecipeModal
+        open={open}
+        onDeleteClicked={async () => {
+          const id: number = Number(recipeId);
+          const deletedRecipe: boolean = await deleteRecipe({ recipeId: id });
+
+          if (!deletedRecipe) {
+            return;
+          }
+
+          onDelete(recipeId);
+          setOpen(false);
+        }}
+        onOpenChange={setOpen}
+      />
+    </>
+  );
+};
+
 export const columns = (
   onDelete: (id: string) => void
 ): ColumnDef<Recipe>[] => [
@@ -58,54 +111,8 @@ export const columns = (
   {
     accessorKey: "actions",
     header: "Actions",
-    cell: ({ row }) => {
-      const [open, setOpen] = useState(false);
-
-      return (
-        <>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" className="w-8 h-8 p-0">
-                <span className="sr-only">Open menu</span>
-                <MoreHorizontal className="w-4 h-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>Actions</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <Link href={`/m/${row.original.recipeId}/edit`}>
-                <DropdownMenuItem className="cursor-pointer">
-                  Edit
-                </DropdownMenuItem>
-              </Link>
-              <DropdownMenuItem
-                className="cursor-pointer"
-                onSelect={(e) => {
-                  e.preventDefault();
-                  setOpen(true);
-                }}
-              >
-                Delete
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-          <DeleteRecipeModal
-            open={open}
-            onDeleteClicked={async () => {
-              const recipeId: number = Number(row.original.recipeId);
-              const deletedRecipe: boolean = await deleteRecipe({ recipeId });
-
-              if (!deletedRecipe) {
-                return;
-              }
-
-              onDelete(row.original.recipeId);
-              setOpen(false);
-            }}
-            onOpenChange={setOpen}
-          />
-        </>
-      );
-    },
+    cell: ({ row }) => (
+      <ActionsCell recipeId={row.original.recipeId} onDelete={onDelete} />
+    ),
   },
 ];
