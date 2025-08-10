d0193884ecdcbd53a1bd4f2cffdf0f0d472d1a7a
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2025-08-09 22:10:32 -0500

    Implement recipe searching

--- web/src/app/components/ui/search-bar.tsx
+++ web/src/app/components/ui/search-bar.tsx
@@ -1,10 +1,22 @@
 import { Input } from "@/app/components/ui/input";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export const SearchBar = () => {
+  const [query, setQuery] = useState("");
+  const router = useRouter();
+
   return (
     <Input
-        placeholder="Search"
-        className="placeholder:text-gray-200"
+      className="placeholder:text-gray-200"
+      placeholder="Search"
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+      onKeyDown={(e) => {
+        if (e.key === "Enter") {
+          router.push(`/?q=${query}`);
+        }
+      }}
     />
   );
 };
