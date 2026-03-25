a6c687b6c145364c438d9597fabfa9f2fa29c09b
Ryan Turner <31020828+RyanTurner02@users.noreply.github.com>
2026-03-24 20:45:05 -0500

    Create deleteUser repository tests

--- api/test/repository/user-repository.test.ts
+++ api/test/repository/user-repository.test.ts
@@ -5,10 +5,14 @@ import { migrate } from "drizzle-orm/mysql2/migrator";
 import { drizzle, MySql2Database } from "drizzle-orm/mysql2";
 import { user } from "@db/schema";
 import * as UserRepository from "@repository/user-repository";
+import * as RecipeRepository from "@repository/recipe-repository";
 import User from "@model/user";
 import { faker } from "@faker-js/faker";
 import { UserDto } from "@dtos/user-dto";
 import { count } from "drizzle-orm";
+import { RecipeDto } from "@dtos/recipe-dto";
+import { IUserRepository } from "@repository/user-repository";
+import { IRecipeRepository } from "@repository/recipe-repository";
 
 describe("UserRepository", () => {
   jest.setTimeout(30000);
@@ -16,7 +20,8 @@ describe("UserRepository", () => {
   let container: StartedMySqlContainer;
   let pool: Pool;
   let db: MySql2Database<Record<string, never>>;
-  let userRepository: any;
+  let userRepository: IUserRepository;
+  let recipeRepository: IRecipeRepository;
 
   const sampleUser: User = new User(1,
     faker.internet.displayName(),
@@ -50,6 +55,7 @@ describe("UserRepository", () => {
     });
 
     userRepository = UserRepository.createUserRepository({ db });
+    recipeRepository = RecipeRepository.createRecipeRepository({ db });
   });
 
   afterAll(async () => {
@@ -74,7 +80,7 @@ describe("UserRepository", () => {
         .select({ count: count() })
         .from(user))
         .length;
-      const actual: number = await userRepository.createUser(newUser);
+      const actual: number | null = await userRepository.createUser(newUser);
 
       expect(actual).toEqual(expected + 1);
     });
@@ -124,9 +130,9 @@ describe("UserRepository", () => {
 
       const actual: boolean = await userRepository.editName(sampleUser.getId(), name);
 
-      const user: User = await userRepository.getUser(sampleUser.getId());
+      const user: User | null = await userRepository.getUser(sampleUser.getId());
       expect(actual).toBe(true);
-      expect(user.getName()).toBe(name);
+      expect(user!.getName()).toBe(name);
     });
   });
 
@@ -136,9 +142,9 @@ describe("UserRepository", () => {
 
       const actual: boolean = await userRepository.editEmail(sampleUser.getId(), email);
 
-      const user: User = await userRepository.getUser(sampleUser.getId());
+      const user: User | null = await userRepository.getUser(sampleUser.getId());
       expect(actual).toBe(true);
-      expect(user.getEmail()).toBe(email);
+      expect(user!.getEmail()).toBe(email);
     });
   });
 
@@ -148,9 +154,42 @@ describe("UserRepository", () => {
 
       const actual: boolean = await userRepository.editPassword(sampleUser.getId(), password);
 
-      const user: User = await userRepository.getUser(sampleUser.getId());
+      const user: User | null = await userRepository.getUser(sampleUser.getId());
       expect(actual).toBe(true);
-      expect(user.getPassword()).toBe(password);
+      expect(user!.getPassword()).toBe(password);
+    });
+  });
+
+  describe("deleteUser", () => {
+    it("deletes the user", async () => {
+      const actual: boolean = await userRepository.deleteUser(sampleUser.getId());
+
+      expect(actual).toBe(true);
+    });
+
+    it("deletes the user and their recipes", async () => {
+      const userId: number = sampleUser.getId();
+      const recipeDto: RecipeDto = {
+        name: "",
+        prepTime: "",
+        cookTime: "",
+        servings: "",
+        ingredients: "",
+        instructions: "",
+      };
+
+      const recipeId: number | null = await recipeRepository.createRecipe(sampleUser.getId(), recipeDto);
+      const actual: boolean = await userRepository.deleteUser(sampleUser.getId());
+      const createdRecipe: RecipeDto | null = await recipeRepository.getRecipe(userId, recipeId!);
+
+      expect(actual).toBe(true);
+      expect(createdRecipe).toBeUndefined();
+    });
+
+    it("does not delete an invalid user", async () => {
+      const actual: boolean = await userRepository.deleteUser(-1);
+
+      expect(actual).toBe(false);
     });
   });
 });
